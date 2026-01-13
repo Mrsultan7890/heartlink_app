f407dd1ebf4d48153efec8181aff89ce67907442
Mrsultan7890 <khansalahuddin2025@gmail.com>
2026-01-13 14:47:27 +0000

    Add missing provider and form files

---
+++ lib/widgets/forms/auth_form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatelessWidget {
+  final Widget child;
+  
+  const AuthForm({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: child);
+  }
+}
